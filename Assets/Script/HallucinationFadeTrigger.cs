4b472d67b577d11db56f40ed6871a5baf363c63c
Lurlichs <79181595+Lurlichs@users.noreply.github.com>
2023-11-24 03:27:56 +1100

    Hallucination platform start, few sounds
    - hallucination just needs some vfx and it'll work
    
    - tried some level building but idk

---
+++ Assets/Script/HallucinationFadeTrigger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class HallucinationFadeTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject platformModel;
+    private MeshRenderer meshRenderer;
+
+    private void Start()
+    {
+        meshRenderer = platformModel.GetComponent<MeshRenderer>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        meshRenderer.material.DOFade(0, 1);
+    }
+}
